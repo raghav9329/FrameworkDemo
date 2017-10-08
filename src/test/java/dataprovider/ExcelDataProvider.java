a0808fc67ebddd45c6791a5523a2967035e8ad86
Ezio <Ezio@Ezio-PC>
2017-10-08 10:54:46 +0530

    commit

--- src/test/java/dataprovider/ExcelDataProvider.java
+++ src/test/java/dataprovider/ExcelDataProvider.java
@@ -1,5 +1,35 @@
-package dataprovider;
 
-public class ExcelDataProvider {
+	package dataprovider;
 
+	import java.io.File;
+	import java.io.FileInputStream;
+
+	import org.apache.poi.xssf.usermodel.XSSFSheet;
+	import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+	public class ExcelDataProvider {
+	XSSFWorkbook wb;
+	XSSFSheet sheet;
+	public ExcelDataProvider(){
+		try{
+			File src=new File("./applicationTestData/AppData.xlsx");
+			FileInputStream fis=new FileInputStream(src);
+			wb = new XSSFWorkbook(fis);
+			
+		}catch(Exception e){
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+	public String getSheet(int sheetno,int row,int col){
+		sheet=wb.getSheetAt(sheetno);
+		String data = sheet.getRow(row).getCell(col).getStringCellValue();
+		return data;
+		
+	}
+//	public (int sheetno)
+
+
+	}
 }
